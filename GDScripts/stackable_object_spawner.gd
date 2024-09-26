6acab22f68588fc11120148065fe2732dc2ef996
Cody <codrin.ilie10@gmail.com>
2024-09-26 22:20:22 +0200

    Object spawning
     - Added the object spawning using a script attached to a Node2D, which also serves as the parent of all the object that will be spawned. NOTE: Not the final version of this script as it was only for testing the scene instantiation.

--- GDScripts/stackable_object_spawner.gd
+++ GDScripts/stackable_object_spawner.gd
@@ -14,6 +14,16 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Space"):
 		spawn_object()
 
+# This function gets a random number, then gets a random scene from the array and instantiates it
 func spawn_object() -> void:
+	# Get the random number
 	var random_object_number := rng.randf_range(0, stackableObjectsScenes.size())
-	add_child(stackableObjectsScenes[random_object_number])
+	
+	# Save the instance of the scene in a variable so we can modify certain properties, such as the position in this case
+	var scene_instance = stackableObjectsScenes[random_object_number].instantiate()
+	
+	# Modify the position of the scene (it will be set to the mouse position for now)
+	scene_instance.global_position = get_global_mouse_position()
+	
+	# Add it as child to the Node this script is attached to
+	add_child(scene_instance)
