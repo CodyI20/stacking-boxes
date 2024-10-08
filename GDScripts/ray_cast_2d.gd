8af024a5dfc7d9b8b96839908d9dcfaad0dafd2a
Cody <codrin.ilie10@gmail.com>
2024-10-08 11:08:41 +0200

    Score line finished
     - Modified the raycast script so that it properly calculates the highest point of the stack of objects and snaps to that location
    
     - It works by firstly checking for the latest dropped object
     - And then by calculating each of the sprite corners and deciding which one has the highest Y position of all
     - And then finally setting the global Y position of the raycast to that point

--- GDScripts/ray_cast_2d.gd
+++ GDScripts/ray_cast_2d.gd
@@ -10,8 +10,34 @@ func update_position(object: Node2D) -> void:
 		var collider = get_collider()
 		if collider != object:
 			return
-		if collider.has_node("Icon"):
-			var sprite = collider.get_node("Icon")
-			var sprite_actual_Y_size = sprite.texture.get_size().y * sprite.scale.y
-			global_position.y = collider.global_position.y - sprite_actual_Y_size/2 - 1
-			print_debug("Position of Y is: ", global_position.y)
+		var highest_point = highest_sprite_point(collider)
+		
+		global_position.y = highest_point - 1
+		
+			
+func highest_sprite_point(collider: Object) -> float:
+	var sprite : Sprite2D
+	if collider.has_node("Icon"):
+			sprite = collider.get_node("Icon")
+	else:
+		print_debug("There is no node named <<ICON>> in this object. Make sure to add one!")
+		return 0
+	var half_extents = sprite.texture.get_size() * sprite.scale/2
+	var rotation = collider.rotation
+	
+	var local_corners = [
+		Vector2(-half_extents.x, -half_extents.y),
+		Vector2(half_extents.x, -half_extents.y),
+		Vector2(half_extents.x, half_extents.y),
+		Vector2(-half_extents.x, half_extents.y)
+	]
+	
+	var world_corners = []
+	var highest_corner_y = INF
+	for corner in local_corners:
+		var rotated_corner = corner.rotated(rotation)
+		var world_corner = collider.global_position + rotated_corner
+		if world_corner.y < highest_corner_y:
+			highest_corner_y = world_corner.y
+	
+	return highest_corner_y
